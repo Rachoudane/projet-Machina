f4cb03a0141c1ea8170c371e7dae75da2527e19d
Rachoudane <marouanebelkhdir@hotmail.fr>
2023-04-06 06:15:19 +0200

    mon projet machina pour IBM

---
+++ Machina/Machina.Android/obj/Release/110/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,45 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "Machina.Android.dll" comes first in this list. */
+		"Machina.Android.dll",
+		"Xamarin.AndroidX.AppCompat.AppCompatResources.dll",
+		"Xamarin.AndroidX.AppCompat.dll",
+		"Xamarin.AndroidX.CardView.dll",
+		"Xamarin.AndroidX.Collection.dll",
+		"Xamarin.AndroidX.CoordinatorLayout.dll",
+		"Xamarin.AndroidX.Core.dll",
+		"Xamarin.AndroidX.CursorAdapter.dll",
+		"Xamarin.AndroidX.CustomView.dll",
+		"Xamarin.AndroidX.DrawerLayout.dll",
+		"Xamarin.AndroidX.Fragment.dll",
+		"Xamarin.AndroidX.Legacy.Support.Core.UI.dll",
+		"Xamarin.AndroidX.Lifecycle.Common.dll",
+		"Xamarin.AndroidX.Lifecycle.LiveData.Core.dll",
+		"Xamarin.AndroidX.Lifecycle.ViewModel.dll",
+		"Xamarin.AndroidX.Loader.dll",
+		"Xamarin.AndroidX.RecyclerView.dll",
+		"Xamarin.AndroidX.SwipeRefreshLayout.dll",
+		"Xamarin.AndroidX.ViewPager.dll",
+		"Xamarin.Google.Android.Material.dll",
+		"FormsViewGroup.dll",
+		"Lottie.Android.dll",
+		"Lottie.Forms.dll",
+		"Machina.dll",
+		"Newtonsoft.Json.dll",
+		"Plugin.CurrentActivity.dll",
+		"Plugin.Media.dll",
+		"Plugin.SimpleAudioPlayer.Abstractions.dll",
+		"Plugin.SimpleAudioPlayer.dll",
+		"Xamarin.AndroidX.Activity.dll",
+		"Xamarin.AndroidX.SavedState.dll",
+		"Xamarin.Essentials.dll",
+		"Xamarin.Forms.Core.dll",
+		"Xamarin.Forms.Platform.Android.dll",
+		"Xamarin.Forms.Platform.dll",
+		"Xamarin.Forms.Xaml.dll",
+		"Xamarin.Google.Guava.ListenableFuture.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
